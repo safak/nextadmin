8121dba071aa9aa198b0464a811c2fc390cde490
CodeLou <tamargo.louiel@gmail.com>
2024-01-11 13:42:29 +0800

    hover effect in menuLink

--- app/ui/dashboard/sidebar/menuLink/menuLink.jsx
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -1,9 +1,19 @@
+"use client";
 import Link from "next/link";
 import styles from "./menuLink.module.css";
+import { usePathname } from "next/navigation";
 
 const MenuLink = ({ item }) => {
+  const pathname = usePathname();
+
+  console.log(pathname);
   return (
-    <Link href={item.path} className={styles.container}>
+    <Link
+      href={item.path}
+      className={`${styles.container} ${
+        pathname === item.path && styles.active
+      }`}
+    >
       {item.icon}
       {item.title}
     </Link>
