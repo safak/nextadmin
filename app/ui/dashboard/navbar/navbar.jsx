139cd6deecbe98337af83ce423f5b9a5977eb866
CodeLou <tamargo.louiel@gmail.com>
2024-01-11 15:08:10 +0800

    style navbar

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,7 +1,32 @@
+"use client";
+import { usePathname } from "next/navigation";
 import styles from "./navbar.module.css";
+import {
+  MdNotifications,
+  MdPublic,
+  MdSearch,
+  MdOutlineChat,
+} from "react-icons/md";
 
 const NavBar = () => {
-  return <div className={styles.container}>NavBar Page</div>;
+  const pathname = usePathname();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>{pathname.split("/").pop()}</div>
+      <div className={styles.menu}>
+        <div className={styles.search}>
+          <MdSearch />
+          <input type="text" placeholder="Search..." className={styles.input} />
+        </div>
+        <div className={styles.icons}>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- app/ui/dashboard/sidebar/menuLink/menuLink.jsx
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -6,7 +6,6 @@ import { usePathname } from "next/navigation";
 const MenuLink = ({ item }) => {
   const pathname = usePathname();
 
-  console.log(pathname);
   return (
     <Link
       href={item.path}
