62cf516270382fa6622aba08435b4f9bf11baeb1
CodeLou <tamargo.louiel@gmail.com>
2024-01-11 15:46:27 +0800

    card component style

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,26 @@
+import Card from "../ui/dashboard/card/Card";
+import Chart from "../ui/dashboard/chart/Chart";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Rightbar from "../ui/dashboard/rightbar/Rightbar";
+import Transactions from "../ui/dashboard/transactions/Transactions";
+
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- app/ui/dashboard/card/Card.jsx
+++ app/ui/dashboard/card/Card.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./card.module.css";
+import { MdSupervisedUserCircle } from "react-icons/md";
 
 const Card = () => {
-  return <div className={styles.container}>Card</div>;
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>total of users</span>
+        <span className={styles.number}>10.234</span>
+        <span className={styles.details}>
+          <span className={styles.positive}>12%</span> more than
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
