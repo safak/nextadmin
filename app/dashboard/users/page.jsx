cf21ae77bfb61f926e1a6664db6662dff9eeafce
CodeLou <tamargo.louiel@gmail.com>
2024-01-16 13:14:42 +0800

    added search component and products, users folder structure

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Search from "@/app/ui/dashboard/search/search";
 
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className={styles.contianer}>
+      <div className={styles.top}>
+        <Search placeholder={"Search for a user"} />
+      </div>
+      <table className={styles.table}></table>
+    </div>
+  );
 };
 
 export default UsersPage;
---
+++ app/ui/dashboard/search/search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { MdSearch } from "react-icons/md";
+import styles from "./search.module.css";
+
+const Search = ({ placeholder }) => {
+  return (
+    <div className={styles.container}>
+      <MdSearch />
+      <input type="text" placeholder={placeholder} className={styles.input} />
+    </div>
+  );
+};
+
+export default Search;
