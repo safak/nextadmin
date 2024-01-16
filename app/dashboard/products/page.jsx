1f1436485644dafda7aedc15041e612f9674c247
CodeLou <tamargo.louiel@gmail.com>
2024-01-16 14:31:39 +0800

    added products page

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder={"Search for a product..."} />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created at</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt="product-image.png"
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                Iphone
+              </div>
+            </td>
+            <td>Description</td>
+            <td>$1523</td>
+            <td>Oct 29, 2023</td>
+            <td>99</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default ProductsPage;
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -10,7 +10,7 @@ const UsersPage = () => {
     <div className={styles.container}>
       <div className={styles.top}>
         <Search placeholder={"Search for a user"} />
-        <Link href="/dashboard/users.add">
+        <Link href="/dashboard/users/add">
           <button className={styles.addButton}>Add</button>
         </Link>
       </div>
